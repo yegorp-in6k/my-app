76bd1cdb1c7c89672a04113c300e7f9079970142
Yegor Platoshkin <yegorp@interlink-ua.com>
2015-05-20 18:37:46 +0300

    OOM

---
+++ src/main/java/com/mycompany/app/MonthCalendar.java
@@ -0,0 +1,15 @@
+package com.mycompany.app;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by employee on 5/20/15.
+ */
+public class MonthCalendar {
+    Week arr[] = new Week[];
+    Calendar date;
+    MonthCalendar(Calendar date){
+        this.date=date;
+    }
+}
